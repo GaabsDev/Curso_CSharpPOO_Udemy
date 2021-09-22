8ea9aff988b822f28bd33524ccadaa24903bfd87
GaabsDev <gabrieldealmeidapires@gmail.com>
2021-09-21 22:17:23 -0300

    Projetos
    Primeiros e Segundo Projeto Base

---
+++ Course/Course/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            Console.WriteLine(false);
+
+            SByte x = 100;
+
+            Console.WriteLine(x);
+
+            byte n1 = 255;
+
+            n1++;
+
+            Console.WriteLine(n1);
+        }
+    }
+}
